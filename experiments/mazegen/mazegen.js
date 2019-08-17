e2476412dc99b7cc322066f1b11b949c6192c096
Jacob <handsjacob@gmail.com>
2019-08-17 17:35:16 +0100

    Added DFS pathfinding to mazegen

--- experiments/mazegen/mazegen.js
+++ experiments/mazegen/mazegen.js
@@ -91,8 +91,55 @@ function generateMazeGrowingTreeStep(map) {
 	}
 }
 
+function startPathfindDepthFirst(map, startx, starty, endx, endy) {
+	map.pathfindStartX = startx
+	map.pathfindStartY = starty
+	map.pathfindEndX = endx
+	map.pathfindEndY = endy
+	map.path = [[startx, starty]]
+	map.visited = []
+	for (var x=0; x<map.width; x++) {
+		var col = []
+		for (var y=0; y<map.height; y++) {
+			col.push(false)
+		}
+		map.visited.push(col)
+	}
+	map.visited[startx][starty] = true
+}
+
+function stepPathfindDepthFirst(map) {
+	var currentCell = map.path[map.path.length-1]
+	var adjacents = map.listOpenAdjacents(currentCell[0], currentCell[1])
+	var advanced = false
+	for (var i=0; i<adjacents.length; i++) {
+		if (!map.visited[adjacents[i][0]][adjacents[i][1]]) {
+			advanced = true
+			map.path.push(adjacents[i])
+			map.visited[adjacents[i][0]][adjacents[i][1]] = true
+			if (adjacents[i][0]==map.pathfindEndX && adjacents[i][1]==map.pathfindEndY) {
+				return true;
+			}
+			break;
+		}
+	}
+	if (!advanced) {
+		map.path.pop()
+		if (map.path.length == 0) {
+			console.log("Error! Could not find path.")
+			return true;
+		}
+	}
+	return false;
+}
+
 Map = {}
 Map.cells = []
+Map.path = []
+Map.pathfindStartX = -1
+Map.pathfindStartY = -1
+Map.pathfindEndX = -1
+Map.pathfindEndY = -1
 Map.lastCellX = -1
 Map.lastCellY = -1
 Map.intervalID = -1
@@ -102,6 +149,7 @@ Map.cellList = []
 Map.width = 0
 Map.height= 0
 Map.isGenerating = false
+Map.isPathfinding = false
 Map.rng = {}
 Map.initialise = function(walls = 0b1111) {
 	this.rng = new Math.seedrandom(document.getElementById("seed").value);
@@ -119,6 +167,23 @@ Map.initialise = function(walls = 0b1111) {
 		var col = []
 		for (var y=0; y<this.height; y++) {
 			col.push(false)
+		}
+		this.visited.push(col)
+	}
+
+	this.path = []
+	this.pathfindStartX = -1
+	this.pathfindStartY = -1
+	this.pathfindEndX = -1
+	this.pathfindEndY = -1
+}
+
+Map.visitAll = function() {
+	this.visited = []
+	for (var x=0; x<this.width; x++) {
+		var col = []
+		for (var y=0; y<this.height; y++) {
+			col.push(true)
 		}
 		this.visited.push(col)
 	}
@@ -150,14 +215,23 @@ Map.startGen = function() {
 	return this.startGenFunction(this, this.rng)
 }
 
-Map.stopGenerating = function() {
+Map.startPathfind = function(startx, starty, endx, endy) {
+	return this.startPathfindFunction(this, startx, starty, endx, endy, this.rng)
+}
+
+Map.clearInterval = function() {
 	clearInterval(this.intervalID)
 	this.intervalID = -1
 	this.isGenerating = false
+	this.isPathfinding = false
 }
 
 Map.step = function() {
 	return this.stepFunction(this, this.rng)
+}
+
+Map.stepPathfind = function() {
+	return this.stepPathfindFunction(this, this.rng)
 }
 
 Map.updateUPS = function() {
@@ -166,15 +240,38 @@ Map.updateUPS = function() {
 		this.intervalID = setInterval(function(map) {
 			done = map.step()
 			if (done) {
-				map.stopGenerating()
+				map.clearInterval()
+			}
+			drawMap(map)
+		}, 1000/document.getElementById("ups").value, this)
+	}
+	if (this.isPathfinding) {
+		clearInterval(this.intervalID)
+		this.intervalID = setInterval(function(map) {
+			done = map.stepPathfind()
+			if (done) {
+				map.clearInterval()
 			}
 			drawMap(map)
 		}, 1000/document.getElementById("ups").value, this)
 	}
 }
 
+Map.listOpenAdjacents = function(x,y) {
+	result = []
+	for (var i=0; i<DIRNAMES.length; i++) { 
+		dir = DIRNAMES[i]
+		if (!(this.cells[x][y] & DIRS[dir])) {
+			result.push([x+DX[dir], y+DY[dir]])
+		}
+	}
+	return result
+}
+
 Map.startGenFunction = generateMazeGrowingTreeStart
 Map.stepFunction = generateMazeGrowingTreeStep
+Map.startPathfindFunction = startPathfindDepthFirst
+Map.stepPathfindFunction = stepPathfindDepthFirst
 
 function drawMap(map) {
 	var canvas=document.getElementById("mainCanvas");
@@ -185,6 +282,11 @@ function drawMap(map) {
 	var UNVISITED_COLOUR = "#CCCCCC"
 	var IN_LIST_COLOUR = "#ffe3e3"
 	var LAST_CELL_COLOUR = "#ff0000"
+
+	var PATH_START_COLOUR = "#03befc"
+	var PATH_GOAL_COLOUR = "#03befc"
+	var PATH_END_COLOUR = "#89009c"
+	var PATH_COLOUR = "#df03fc"
 
 	var CELL_WIDTH = Math.floor(canvas.width/map.width);
 	var CELL_HEIGHT = Math.floor(canvas.height/map.height);
@@ -232,31 +334,94 @@ function drawMap(map) {
 			}
 		}
 	}
+
+	if (map.path.length > 0) {
+		ctx.strokeStyle = PATH_COLOUR
+		ctx.lineWidth = Math.max(1, CELL_WIDTH/4)
+		ctx.beginPath()
+		ctx.moveTo(map.path[0][0]*CELL_WIDTH + CELL_WIDTH/2, map.path[0][0]*CELL_HEIGHT + CELL_HEIGHT/2)
+		for (var i=1; i<map.path.length; i++) {
+			var x = map.path[i][0]*CELL_WIDTH + CELL_WIDTH/2
+			var y = map.path[i][1]*CELL_HEIGHT + CELL_HEIGHT/2
+			ctx.lineTo(x,y)
+		}
+		ctx.stroke()
+		ctx.closePath()
+
+		ctx.fillStyle = PATH_END_COLOUR
+		ctx.beginPath()
+		var x = map.path[map.path.length-1][0]*CELL_WIDTH + CELL_WIDTH/2
+		var y = map.path[map.path.length-1][1]*CELL_HEIGHT + CELL_HEIGHT/2
+		ctx.arc(x,y, CELL_WIDTH/4, 0, 2*Math.PI)
+		ctx.fill()
+		ctx.closePath()
+	}
+
+	ctx.fillStyle = PATH_START_COLOUR
+	ctx.beginPath()
+	ctx.arc(map.pathfindStartX*CELL_WIDTH + CELL_WIDTH/2, map.pathfindStartY*CELL_HEIGHT + CELL_HEIGHT/2, CELL_WIDTH/3, 0, 2*Math.PI)
+	ctx.fill()
+	ctx.closePath()
+
+	ctx.fillStyle = PATH_GOAL_COLOUR
+	ctx.beginPath()
+	ctx.arc(map.pathfindEndX*CELL_WIDTH + CELL_WIDTH/2, map.pathfindEndY*CELL_HEIGHT + CELL_HEIGHT/2, CELL_WIDTH/3, 0, 2*Math.PI)
+	ctx.fill()
+	ctx.closePath()
 }
 
 function generateStepwise() {
-	Map.stopGenerating()
+	Map.clearInterval()
 	Map.isGenerating = true
 	Map.startGen()
 	drawMap(Map)
 	Map.intervalID = setInterval(function(map) {
 		done = map.step()
 		if (done) {
-			Map.stopGenerating()
+			Map.clearInterval()
 		}
 		drawMap(map)
 	}, 1000/document.getElementById("ups").value, Map)
 }
 
 function generateAtOnce() {
-	Map.stopGenerating()
-	var rng = new Math.seedrandom(document.getElementById("seed").value);
-	Map.startGen(rng)
+	Map.clearInterval()
+	Map.startGen()
 	done = false
 	while (!done) {
-		done = Map.step(rng)
+		done = Map.step()
 	}
 	Map.isGenerating = false
+	drawMap(Map)
+}
+
+function pathfindStepwise() {
+	generateAtOnce()
+
+	Map.clearInterval()
+	Map.isPathfinding = true
+	Map.startPathfind(0, 0, Map.width-1, Map.height-1)
+	drawMap(Map)
+	Map.intervalID = setInterval(function(map) {
+		done = map.stepPathfind()
+		if (done) {
+			Map.clearInterval()
+		}
+		drawMap(map)
+	}, 1000/document.getElementById("ups").value, Map)
+}
+
+function pathfindAtOnce() {
+	generateAtOnce()
+
+	Map.clearInterval()
+	Map.startPathfind(0, 0, Map.width-1, Map.height-1)
+	done = false
+	while (!done) {
+		done = Map.stepPathfind()
+	}
+	Map.isPathfinding = false
+	Map.visitAll()
 	drawMap(Map)
 }
 
@@ -266,13 +431,16 @@ function updateAll() {
 	document.getElementById("randomProbabilityOut").innerHTML = String(document.getElementById("randomProbability").value)+"%";
 	document.getElementById("upsOut").innerHTML = document.getElementById("ups").value;
 	Map.randomProbability = document.getElementById("randomProbability").value/100
-	Map.stopGenerating()
+	Map.clearInterval()
 	Map.width = size
 	Map.height = size
 	Map.initialise()
 
 	if (document.getElementById("autoGenerate").checked) {
 		generateAtOnce()
+	}
+	if (document.getElementById("autoPathfind").checked) {
+		pathfindAtOnce()
 	}
 
 	drawMap(Map)
@@ -302,9 +470,23 @@ document.getElementById("generateAtOnceButton").addEventListener("click", functi
 	generateAtOnce()
 });
 
+document.getElementById("pathfindStepwiseButton").addEventListener("click", function() {
+	pathfindStepwise()
+});
+
+document.getElementById("pathfindAtOnceButton").addEventListener("click", function() {
+	pathfindAtOnce()
+});
+
 document.getElementById("autoGenerate").addEventListener("change", function() {
 	if (document.getElementById("autoGenerate").checked) {
 		generateAtOnce()
+	}
+});
+
+document.getElementById("autoPathfind").addEventListener("change", function() {
+	if (document.getElementById("autoPathfind").checked) {
+		pathfindAtOnce()
 	}
 });
 
